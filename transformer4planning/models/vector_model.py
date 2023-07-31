3e6132166e639099224472f047f9910b361525a1
QJ00367-122 <QJ00367@example.com>
2023-07-31 15:50:49 +0800

    update vector_model with keypoints setting

--- transformer4planning/models/vector_model.py
+++ transformer4planning/models/vector_model.py
@@ -1,3 +1,4 @@
+from transformers import GPT2Model, GPT2PreTrainedModel, GPT2Tokenizer
 import torch
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss, SmoothL1Loss
 from transformer4planning.models.GPT2.models import *
@@ -44,6 +45,11 @@ class GPTNonAutoRegressiveModelVector(GPT2PreTrainedModel):
         self.model_parallel = False
         self.device_map = None
         self.with_traffic_light = model_args.with_traffic_light
+        
+        if self.model_args.token_scenario_tag:
+            self.tag_tokenizer = GPT2Tokenizer.from_pretrained(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gpt2-tokenizer'))
+            self.tag_tokenizer.pad_token = self.tag_tokenizer.eos_token
+            self.tag_embedding = nn.Embedding(self.tag_tokenizer.vocab_size, config.n_embd)
         self.post_init()
 
     @add_start_docstrings(PARALLELIZE_DOCSTRING)
@@ -142,21 +148,43 @@ class GPTNonAutoRegressiveModelVector(GPT2PreTrainedModel):
         input_embeds[:, 1::2, :] = action_embeds  # index: 1, 3, 5, .., 19
 
 
+        if self.model_args.token_scenario_tag:
+            scenario_type = kwargs.get("scenario_type", None)
+            scenario_tag_ids = list()
+            for i in range(batch_size):
+                scenario_tag_ids.append(torch.tensor(self.tag_tokenizer(scenario_type[i], max_length=self.model_args.max_token_len, padding='max_length')["input_ids"]).unsqueeze(0))
+            scenario_tag_ids = torch.stack(scenario_tag_ids, dim=0).to(device)
+            scenario_tag_embeds = self.tag_embedding(scenario_tag_ids).squeeze(1)
+            assert scenario_tag_embeds.shape[1] == self.model_args.max_token_len, f'{scenario_tag_embeds.shape} vs {self.model_args.max_token_len}'
+            input_embeds = torch.cat([scenario_tag_embeds, input_embeds], dim=1)
+
         if self.ar_future_interval == 0:
             # to keep input and output at the same dimension
-            input_embeds = torch.cat([input_embeds, torch.zeros((batch_size, pred_length, n_embed), device=device)], dim=1)
+            input_embeds = torch.cat([input_embeds,
+                                      torch.zeros((batch_size, pred_length, n_embed), device=device)], dim=1)
             # attention_mask = torch.ones((input_embeds.shape[0], input_embeds.shape[1]), device=device)
             # attention_mask[:, context_length * 2:] = 0
         elif self.ar_future_interval > 0:
             # use autoregressive future interval
-            future_key_points = trajectory_label[:, self.ar_future_interval-1::self.ar_future_interval, :]
+            if self.model_args.specified_key_points:
+                # 80, 40, 20, 10, 5
+                if self.model_args.forward_specified_key_points:
+                    selected_indices = [4, 9, 19, 39, 79]
+                else:
+                    selected_indices = [79, 39, 19, 9, 4]
+                future_key_points = trajectory_label[:, selected_indices, :]
+            else:
+                future_key_points = trajectory_label[:, self.ar_future_interval - 1::self.ar_future_interval, :]
             assert future_key_points.shape[1] != 0, 'future points not enough to sample'
 
             future_key_points_aug = future_key_points.clone()
             if self.model_args.arf_x_random_walk > 0 and self.training:
                 x_noise = torch.rand(future_key_points.shape, device=device) * self.model_args.arf_x_random_walk * 2 - self.model_args.arf_x_random_walk
                 # add progressive scale, the future points the larger noise
-                indices = torch.arange(future_key_points.shape[1], device=device) / future_key_points.shape[1]
+                if self.model_args.specified_key_points:
+                    indices = torch.tensor(selected_indices, device=device, dtype=float) / 80.0
+                else:
+                    indices = torch.arange(future_key_points.shape[1], device=device) / future_key_points.shape[1]
                 expanded_indices = indices.unsqueeze(0).unsqueeze(-1).expand(future_key_points.shape)
                 x_noise = x_noise * expanded_indices
                 future_key_points_aug[:, :, 0] += x_noise[:, :, 0]
@@ -171,7 +199,8 @@ class GPTNonAutoRegressiveModelVector(GPT2PreTrainedModel):
                 future_key_points_aug[:, :, 2:] = 0
 
             future_key_embeds = self.action_m_embed(future_key_points_aug)
-            input_embeds = torch.cat([input_embeds, future_key_embeds, torch.zeros((batch_size, pred_length, n_embed), device=device)], dim=1)
+            input_embeds = torch.cat([input_embeds, future_key_embeds,
+                                      torch.zeros((batch_size, pred_length, n_embed), device=device)], dim=1)
             # attention_mask = torch.ones((input_embeds.shape[0], input_embeds.shape[1]), device=device)
             # attention_mask[:, context_length * 2 + future_key_embeds.shape[1]:] = 0
         else:
@@ -194,25 +223,30 @@ class GPTNonAutoRegressiveModelVector(GPT2PreTrainedModel):
 
         transformer_outputs_hidden_state = transformer_outputs['last_hidden_state']
 
-        traj_hidden_state = transformer_outputs_hidden_state[:, -pred_length-1:-1, :]
+        traj_hidden_state = transformer_outputs_hidden_state[:, -pred_length - 1:-1, :]
         # expected shape for pred trajectory is (b, pred_length, 4)
-        traj_logits = self.traj_decoder(traj_hidden_state)
         loss = torch.tensor(0, dtype=torch.float32, device=device)
-
         if 'mse' in self.loss_fn:
-            loss_fct = MSELoss(reduction="mean")
+            loss_fct = nn.MSELoss(reduction="mean")
         elif 'l1' in self.loss_fn:
-            loss_fct = SmoothL1Loss()
-        if self.task == "waymo":
-            loss_fct = MSELoss(reduction="none")
-            y_mask = ((trajectory_label!=-1).sum(-1)>0).view(batch_size, pred_length, 1)
-            _loss = (loss_fct(traj_logits[...,:2], trajectory_label[...,:2].to(device))*y_mask).sum()/(y_mask.sum()+1e-7)
-            loss += _loss
-        else:
-            if self.model_args.predict_yaw:
-                loss += loss_fct(traj_logits, trajectory_label.to(device)) * self.model_args.trajectory_loss_rescale
+            loss_fct = nn.SmoothL1Loss()
+        if not self.model_args.pred_key_points_only:
+            traj_logits = self.traj_decoder(traj_hidden_state)
+            if self.task == "waymo":
+                loss_fct = MSELoss(reduction="none")
+                y_mask = torch.ones((batch_size, pred_length, 1), dtype=torch.bool, device=trajectory_label.device)
+                # y_mask = ((trajectory_label != -1).sum(-1) > 0).view(batch_size, pred_length, 1)
+                _loss = (loss_fct(traj_logits[..., :2], trajectory_label[..., :2].to(device)) * y_mask).sum() / (
+                            y_mask.sum() + 1e-7)
+                loss += _loss
             else:
-                loss += loss_fct(traj_logits[..., :2], trajectory_label[..., :2].to(device)) * self.model_args.trajectory_loss_rescale
+                if self.model_args.predict_yaw:
+                    loss += loss_fct(traj_logits, trajectory_label.to(device)) * self.model_args.trajectory_loss_rescale
+                else:
+                    loss += loss_fct(traj_logits[..., :2], trajectory_label[...,
+                                                           :2].to(device)) * self.model_args.trajectory_loss_rescale
+        else:
+            traj_logits = torch.zeros_like(trajectory_label[..., :2])
 
         if self.ar_future_interval > 0:
             """
@@ -222,7 +256,8 @@ class GPTNonAutoRegressiveModelVector(GPT2PreTrainedModel):
             input_embed: [O, A, O, A, FutureKey1, FutureKey2, Traj1(Given0), Traj2(Given0)..]
             output_embed: [A, O, A, FutureKey1, FutureKey2, Traj1, Traj2.., x(Attentionally Blank)]
             """
-            future_key_points_hidden_state = transformer_outputs_hidden_state[:, context_length * 2 - 1:context_length * 2 + future_key_points.shape[1] - 1, :]
+            scenario_type_len = self.model_args.max_token_len if self.model_args.token_scenario_tag else 0
+            future_key_points_hidden_state = transformer_outputs_hidden_state[:, scenario_type_len + context_length * 2 - 1:scenario_type_len + context_length * 2 + future_key_points.shape[1] - 1, :]
             key_points_logits = self.key_points_decoder(future_key_points_hidden_state)  # b, s, 4/2*k
 
             if self.k == 1:
@@ -230,6 +265,10 @@ class GPTNonAutoRegressiveModelVector(GPT2PreTrainedModel):
                     loss_to_add = loss_fct(key_points_logits, future_key_points.to(device))
                 else:
                     loss_to_add = loss_fct(key_points_logits, future_key_points[..., :2].to(device))
+                
+                if self.task == "waymo":
+                    y_mask_add = torch.ones((batch_size, future_key_points.shape[1], 1), dtype=torch.bool, device=trajectory_label.device)
+                    loss_to_add = (loss_to_add* y_mask_add).sum() / (y_mask_add.sum() + 1e-7)
                 loss += loss_to_add
                 traj_logits = torch.cat([key_points_logits, traj_logits], dim=1)
             else:
@@ -254,10 +293,12 @@ class GPTNonAutoRegressiveModelVector(GPT2PreTrainedModel):
                     loss += loss_to_add
                     if self.training:
                         # concatenate the key points with predicted trajectory for evaluation
-                        selected_key_points = key_points_logits.reshape(b*s, self.k, -1)[torch.arange(b*s), min_loss_indices.reshape(-1), :].reshape(b, s, -1)
+                        selected_key_points = key_points_logits.reshape(b * s, self.k, -1)[torch.arange(b * s),
+                                              min_loss_indices.reshape(-1), :].reshape(b, s, -1)
                     else:
                         # concatenate the key points with predicted trajectory selected from the classifier for evaluation
-                        selected_key_points = key_points_logits.reshape(b*s, self.k, -1)[torch.arange(b*s), pred_logits.argmax(dim=-1).reshape(-1), :].reshape(b, s, -1)
+                        selected_key_points = key_points_logits.reshape(b * s, self.k, -1)[torch.arange(b * s),
+                                              pred_logits.argmax(dim=-1).reshape(-1), :].reshape(b, s, -1)
                     traj_logits = torch.cat([selected_key_points, traj_logits], dim=1)
                 else:
                     print('WARNING: Randomly select key points for evaluation, try to use next_token_scorer_decoder')
