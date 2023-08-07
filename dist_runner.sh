d24c8fb2d9eb19527a743a45f9e78f2c9e8d438e
QJ00367-122 <QJ00367@example.com>
2023-08-07 16:33:37 +0800

    update script"

--- dist_runner.sh
+++ dist_runner.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7; \
-python -m torch.distributed.run --nproc_per_node=7 runner_waymo.py \
+python -m torch.distributed.run --nproc_per_node=7 --master_port=29502 runner_waymo.py \
         --model_name scratch-vector-gpt --model_pretrain_name_or_path None \
         --saved_dataset_folder  /data_3/madanjiao/nuplan/online_demo/mini_demo/index \
-        --output_dir /data_3/madanjiao/model_res/vector_gpt_small_k2_reverseKP5_masked_ep100_refactor_cls4/training_results  \
-        --logging_dir /data_3/madanjiao/model_res/vector_gpt_small_k2_reverseKP5_masked_ep100_refactor_cls4/training_logs \
+        --output_dir /data_3/madanjiao/model_res/vector_gpt_small_k6_reverseKP5_masked_ep100_cls10/training_results  \
+        --logging_dir /data_3/madanjiao/model_res/vector_gpt_small_k6_reverseKP5_masked_ep100_cls10/training_logs \
         --run_name gpt_1.5B_mse_FI1_PI1_k1 --num_train_epochs 100 \
         --per_device_train_batch_size 16 --warmup_steps 50 \
-        --weight_decay 0.01 --logging_steps 2 --save_strategy steps \
+        --weight_decay 0.01 --logging_steps 100 --save_strategy steps \
         --save_steps 5000 --dataloader_num_workers 10 \
         --save_total_limit 50  --predict_trajectory True --ar_future_interval 5 --specified_key_points True\
         --dataloader_drop_last True --do_train \
         --d_embed 256 --d_model 256 --d_inner 1024 --n_layers 4 --n_heads 4 \
         --activation_function silu --dataset_scale 1 \
-        --task waymo --with_traffic_light True --k 2 \
+        --task waymo --with_traffic_light True --k 6 \
         --online_preprocess True \
         --datadic_path /data_3/madanjiao/nuplan/online_demo/mini_demo \
         --remove_unused_columns False --future_sample_interval 1 \
