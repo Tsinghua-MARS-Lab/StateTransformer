c29dc6d10adb8ed3407f3db376d560667959ccc8
QJ00367-122 <QJ00367@example.com>
2023-08-17 10:42:43 +0800

    update script

--- dist_eval.sh
+++ dist_eval.sh
@@ -3,8 +3,8 @@
 export CUDA_VISIBLE_DEVICES=0;
 python -m torch.distributed.run --nproc_per_node=1 --master_port=29501 runner_waymo.py \
         --model_name pretrain-vector-gpt \
-        --model_pretrain_name_or_path /data_3/madanjiao/model_res/vector_gpt_small_k6_reverseKP5_correctA_ep100_cls10/training_results/checkpoint-180000 \
-        --output_dir /data_3/madanjiao/model_res/vector_gpt_small_k6_reverseKP5_correctA_ep100_cls10/training_results  \
+        --model_pretrain_name_or_path /data_3/madanjiao/model_res/vector_gpt_small_k6_reverseKP5_correctA_kptsLogitsCls_ep100/training_results/checkpoint-160000 \
+        --output_dir /data_3/madanjiao/model_res/vector_gpt_small_k6_reverseKP5_correctA_kptsLogitsCls_ep100/training_results  \
         --run_name gpt_mse_FI1_PI1_k1_test \
         --per_device_eval_batch_size 16 --dataloader_num_workers 10 --predict_trajectory True --ar_future_interval 5 --specified_key_points True --forward_specified_key_points False \
         --dataloader_drop_last True \
