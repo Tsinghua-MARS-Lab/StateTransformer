8c37f925eb562e28472cf3c9e45b856efa05e6ee
QJ00367 <QJ00367@example.com>
2023-08-24 16:49:06 +0800

    modify script to remove unnecessary params

--- dist_eval.sh
+++ dist_eval.sh
@@ -18,7 +18,6 @@ python -m torch.distributed.run --nproc_per_node=1 --master_port=29501 runner_wa
 # python -m torch.distributed.run --nproc_per_node=1 --master_port=29501 runner_waymo.py \
 #         --model_name pretrain-vector-gpt \
 #         --model_pretrain_name_or_path /data_3/madanjiao/model_res/vector_gpt_small_k6_forwardKP5_ep100/training_results/checkpoint-30000 \
-#         --saved_dataset_folder  /data_3/madanjiao/nuplan/online_demo/mini_demo/index \
 #         --output_dir /data_3/madanjiao/model_res/vector_gpt_small_k6_forwardKP5_ep100/training_results  \
 #         --run_name gpt_mse_FI1_PI1_k1_test \
 #         --per_device_eval_batch_size 16 --dataloader_num_workers 10 --ar_future_interval 5 --specified_key_points True --forward_specified_key_points True \
@@ -27,8 +26,6 @@ python -m torch.distributed.run --nproc_per_node=1 --master_port=29501 runner_wa
 #         --activation_function silu --dataset_scale 1 \
 #         --task waymo \
 #         --with_traffic_light True --k 6 \
-#         --datadic_path /data_3/madanjiao/nuplan/online_demo/mini_demo \
 #         --remove_unused_columns False --future_sample_interval 1 \
 #         --past_sample_interval 1 --do_eval\
-#         --saved_valid_dataset_folder /data_3/madanjiao/nuplan/online_demo/mini_demo/index \
 #         --overwrite_output_dir --loss_fn mse
--- dist_runner.sh
+++ dist_runner.sh
@@ -14,17 +14,13 @@ python -m torch.distributed.run --nproc_per_node=7 --master_port=29502 runner_wa
         --dataloader_drop_last True --do_train \
         --d_model 256 --dataset_scale 1 \
         --task waymo --with_traffic_light True --k 1 \
-        --online_preprocess True \
-        --datadic_path /data_3/madanjiao/nuplan/online_demo/mini_demo \
         --remove_unused_columns False --future_sample_interval 1 \
         --past_sample_interval 1 \
-        --saved_valid_dataset_folder /data_3/madanjiao/nuplan/online_demo/mini_demo/index \
         --overwrite_output_dir --loss_fn mse
 
 # export CUDA_VISIBLE_DEVICES=0; \
 # python -m torch.distributed.run --nproc_per_node=1 --master_port=29510 runner_waymo.py \
 #         --model_name scratch-vector-gpt --model_pretrain_name_or_path None  \
-#         --saved_dataset_folder  /data/madanjiao/nuplan/online_demo/mini_demo/index \
 #         --output_dir /data/madanjiao/model_res/vector_gpt_small_k1_KP0_anchored_ep100_vehicle/training_results  \
 #         --logging_dir /data/madanjiao/model_res/vector_gpt_small_k1_KP0_anchored_ep100_vehicle/training_logs \
 #         --run_name gpt_1.5B_mse_FI1_PI1_k1 --num_train_epochs 100 \
@@ -35,11 +31,8 @@ python -m torch.distributed.run --nproc_per_node=7 --master_port=29502 runner_wa
 #         --dataloader_drop_last True --do_train \
 #         --d_model 256 --dataset_scale 1 \
 #         --task waymo --with_traffic_light True --k 1 \
-#         --online_preprocess True \
-#         --datadic_path /data_3/madanjiao/nuplan/online_demo/mini_demo \
 #         --remove_unused_columns False --future_sample_interval 1 \
 #         --past_sample_interval 1 \
-#         --saved_valid_dataset_folder /data_3/madanjiao/nuplan/online_demo/mini_demo/index \
 #         --overwrite_output_dir --loss_fn mse
 
     
