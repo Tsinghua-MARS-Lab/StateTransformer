161fbc8e97caae5acbf3b3d375c8432092faa00b
QJ00367-122 <QJ00367@example.com>
2023-09-18 09:58:13 +0800

    update mtr util

--- transformer4planning/models/utils.py
+++ transformer4planning/models/utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import numpy as np
 import math
 from typing import Optional
@@ -118,13 +119,14 @@ def uniform_positional_embedding(key_point_num, feat_dim):
 
     return pos_embedding
 
-def nll_loss_gmm_direct(pred_trajs, gt_trajs, gt_valid_mask,
+def nll_loss_gmm_direct(pred_scores, pred_trajs, gt_trajs, gt_valid_mask, pre_nearest_mode_idxs=None,
                         timestamp_loss_weight=None, use_square_gmm=False, log_std_range=(-1.609, 5.0), rho_limit=0.5):
     """
     GMM Loss for Motion Transformer (MTR): https://arxiv.org/abs/2209.13508
     Written by Shaoshuai Shi 
 
     Args:
+        pred_scores (batch_size, num_modes):
         pred_trajs (batch_size, num_modes, num_timestamps, 5 or 3)
         gt_trajs (batch_size, num_timestamps, 2):
         gt_valid_mask (batch_size, num_timestamps):
@@ -135,7 +137,18 @@ def nll_loss_gmm_direct(pred_trajs, gt_trajs, gt_valid_mask,
     else:
         assert pred_trajs.shape[-1] == 5
 
-    nearest_trajs = pred_trajs  # (batch_size, num_timestamps, 5)
+    batch_size = pred_scores.shape[0]
+
+    if pre_nearest_mode_idxs is not None:
+        nearest_mode_idxs = pre_nearest_mode_idxs
+    else:
+        distance = (pred_trajs[:, :, :, 0:2] - gt_trajs[:, None, :, :]).norm(dim=-1) 
+        distance = (distance * gt_valid_mask[:, None, :]).sum(dim=-1) 
+
+        nearest_mode_idxs = distance.argmin(dim=-1)
+    nearest_mode_bs_idxs = torch.arange(batch_size).type_as(nearest_mode_idxs)  # (batch_size, 2)
+
+    nearest_trajs = pred_trajs[nearest_mode_bs_idxs, nearest_mode_idxs]  # (batch_size, num_timestamps, 5)
     res_trajs = gt_trajs - nearest_trajs[:, :, 0:2]  # (batch_size, num_timestamps, 2)
     dx = res_trajs[:, :, 0]
     dy = res_trajs[:, :, 1]
@@ -151,7 +164,7 @@ def nll_loss_gmm_direct(pred_trajs, gt_trajs, gt_valid_mask,
         std2 = torch.exp(log_std2)  # (0.2m to 150m)
         rho = torch.clip(nearest_trajs[:, :, 4], min=-rho_limit, max=rho_limit)
 
-    gt_valid_mask = gt_valid_mask.type_as(pred_trajs)
+    gt_valid_mask = gt_valid_mask.type_as(pred_scores)
     if timestamp_loss_weight is not None:
         gt_valid_mask = gt_valid_mask * timestamp_loss_weight[None, :]
 
@@ -161,4 +174,20 @@ def nll_loss_gmm_direct(pred_trajs, gt_trajs, gt_valid_mask,
 
     reg_loss = ((reg_gmm_log_coefficient + reg_gmm_exp) * gt_valid_mask).sum(dim=-1)
 
-    return reg_loss
+    return reg_loss, nearest_mode_idxs
+
+def build_mlps(c_in, mlp_channels=None, ret_before_act=False, without_norm=False):
+    layers = []
+    num_layers = len(mlp_channels)
+
+    for k in range(num_layers):
+        if k + 1 == num_layers and ret_before_act:
+            layers.append(nn.Linear(c_in, mlp_channels[k], bias=True))
+        else:
+            if without_norm:
+                layers.extend([nn.Linear(c_in, mlp_channels[k], bias=True), nn.ReLU()]) 
+            else:
+                layers.extend([nn.Linear(c_in, mlp_channels[k], bias=False), nn.BatchNorm1d(mlp_channels[k]), nn.ReLU()])
+            c_in = mlp_channels[k]
+
+    return nn.Sequential(*layers)
