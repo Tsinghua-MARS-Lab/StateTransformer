39637687f27c928ca4d41c311a555fe1badb2277
Shiduo-zh <1372286707@qq.com>
2023-10-10 19:49:26 +0800

    del old absolute value argumentation

--- transformer4planning/models/encoder/augmentation.py
+++ transformer4planning/models/encoder/augmentation.py
@@ -10,14 +10,14 @@ class DataAugmentation(nn.Module):
     def __init__(self):
         super().__init__()
     
-    def trajectory_augmentation(self, target_traj, x_noise_scale, y_noise_scale, expanded_indices=1):
-        if self.training and x_noise_scale > 0:
-            x_noise = (torch.rand(target_traj.shape, device=target_traj.device) * x_noise_scale * 2 - x_noise_scale) * expanded_indices
-            target_traj[..., 0] += x_noise[..., 0] 
-        if self.training and y_noise_scale > 0:
-            y_noise = (torch.rand(target_traj.shape, device=target_traj.device) * y_noise_scale * 2 - y_noise_scale) * expanded_indices
-            target_traj[..., 1] += y_noise[..., 1]
-        return target_traj
+    # def trajectory_augmentation(self, target_traj, x_noise_scale, y_noise_scale, expanded_indices=1):
+    #     if self.training and x_noise_scale > 0:
+    #         x_noise = (torch.rand(target_traj.shape, device=target_traj.device) * x_noise_scale * 2 - x_noise_scale) * expanded_indices
+    #         target_traj[..., 0] += x_noise[..., 0] 
+    #     if self.training and y_noise_scale > 0:
+    #         y_noise = (torch.rand(target_traj.shape, device=target_traj.device) * y_noise_scale * 2 - y_noise_scale) * expanded_indices
+    #         target_traj[..., 1] += y_noise[..., 1]
+    #     return target_traj
 
     def trajectory_linear_augmentation(self, target_traj, x_noise_scale, y_noise_scale, reverse_scale=True):
         """
